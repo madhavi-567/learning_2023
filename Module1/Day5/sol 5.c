814e8f32b87932c7932e63c87df4c6ed186dae6f
madhavi-567 <136503355+madhavi-567@users.noreply.github.com>
2023-06-21 16:06:54 +0530

    solution5

---
+++ Module1/Day5/sol 5.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+// Structure to store information
+struct Student {
+    char name[50];
+    int age;
+    float marks;
+};
+
+// Function to swap the fields of two structures
+void swapFields(struct Student *student1, struct Student *student2) {
+    struct Student temp = *student1;
+    *student1 = *student2;
+    *student2 = temp;
+}
+
+int main() {
+    struct Student student1, student2;
+
+    printf("Enter details for student 1:\n");
+    printf("Name: ");
+    scanf("%s", student1.name);
+
+    printf("Age: ");
+    scanf("%d", &student1.age);
+
+    printf("Marks: ");
+    scanf("%f", &student1.marks);
+
+    printf("\nEnter details for student 2:\n");
+    printf("Name: ");
+    scanf("%s", student2.name);
+
+    printf("Age: ");
+    scanf("%d", &student2.age);
+
+    printf("Marks: ");
+    scanf("%f", &student2.marks);
+
+    printf("\nBefore swapping:\n");
+    printf("Student 1:\n");
+    printf("Name: %s\n", student1.name);
+    printf("Age: %d\n", student1.age);
+    printf("Marks: %.2f\n", student1.marks);
+
+    printf("\nStudent 2:\n");
+    printf("Name: %s\n", student2.name);
+    printf("Age: %d\n", student2.age);
+    printf("Marks: %.2f\n", student2.marks);
+
+    // Swap the fields using pointers
+    swapFields(&student1, &student2);
+
+    printf("\nAfter swapping:\n");
+    printf("Student 1:\n");
+    printf("Name: %s\n", student1.name);
+    printf("Age: %d\n", student1.age);
+    printf("Marks: %.2f\n", student1.marks);
+
+    printf("\nStudent 2:\n");
+    printf("Name: %s\n", student2.name);
+    printf("Age: %d\n", student2.age);
+    printf("Marks: %.2f\n", student2.marks);
+
+    return 0;
+}
